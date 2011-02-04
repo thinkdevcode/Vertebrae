fa9569b28e38a0e623b5486a547bd3d9875319f8
gene <gin4lyfe@gmail.com>
2011-02-03 22:50:01 -0500

    Initial commit - will be adding a bunch more

---
+++ Vertebrae/Vertebrae.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Collections;
+
+namespace Vertebrae
+{
+    public partial class Vertebrae : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static ArrayList GetTestArray()
+        {
+            return new ArrayList() 
+            {
+                new { Name = "Test1", Age = "1337" },
+                new { Name = "Test2", Age = "22" }
+            };
+        }
+    }
+}
---
+++ Vertebrae/Vertebrae.js
@@ -0,0 +1,112 @@
+﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4-vsdoc.js" />
+
+(function (window, undefined) {
+
+    var root = this,
+
+    vertebrae = {},
+
+    view = {
+
+        view: {
+
+            add: function (name, jq) {
+                this.cache = this.cache || {};
+                if (!this.cache[name]) {
+                    this.cache[name] = jq;
+                }
+            },
+
+            upd: function (name, jq) {
+                if (this.cache && this.cache[name]) {
+                    this.cache[name] = jq;
+                }
+            },
+
+            get: function (name) {
+                if (this.cache) {
+                    if (this.cache[name]) {
+                        return this.cache[name];
+                    }
+                }
+            }
+        }
+    },
+
+    data = {
+
+        data: {
+
+            addHandler: function (name, url) {
+                if (!this[name]) {
+                    this[name] = function (obj, succ, err, pre) {
+                        $.ajax({
+                            type: "POST",
+                            url: url,
+                            data: (obj === null) ? '{}' : JSON.stringify(obj),
+                            contentType: "application/json; charset=utf-8",
+                            dataType: "json",
+                            beforeSend: pre || function () { },
+                            error: err || function () { },
+                            success: function (e) {
+                                if (typeof succ === 'function') {
+                                    if (e.d instanceof Array) { succ(e.d); }
+                                    else { succ(JSON.parse(e.d)); }
+                                }
+                            }
+                        });
+                    };
+                }
+            }
+        }
+
+    },
+
+    event = {
+
+        event: {
+
+            addCustom: function (evntName, hndlrName) {
+                this.custcache = this.custcache || {};
+                var hndlr = this.getHandler(hndlrName);
+                if (typeof hndlr === 'function') {
+                    this.custcache[evntName] = this.custcache[evntName] || [];
+                    this.custcache[evntName].push(hndlr);
+                }
+            },
+
+            fire: function (evntName) {
+                if (this.custcache && this.custcache[evntName]) {
+                    _.each(this.custcache[evntName], function (fn) { fn(); });
+                }
+            },
+
+            addHandler: function (name, hndlr) {
+                this.cache = this.cache || {};
+                if (!this.cache[name]) {
+                    this.cache[name] = hndlr;
+                }
+            },
+
+            getHandler: function (name) {
+                if (this.cache[name]) {
+                    return this.cache[name];
+                }
+            },
+
+            add: function (ctrlName, evntName, hndlrName) {
+                var hndlr = this.getHandler(hndlrName);
+                if (typeof hndlr === 'function') {
+                    var ctrl = view.view.get(ctrlName);
+                    if (ctrl && typeof ctrl === 'object') {
+                        ctrl.bind(evntName, hndlr);
+                    }
+                }
+            }
+
+        }
+    };
+
+    root.vertebrae = _.extend(vertebrae, view, data, event);
+
+})();
